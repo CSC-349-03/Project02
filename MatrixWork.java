94433ecd0552c16e99af5a2980433048dcf50ac1
Andrew LeDawson <andrew@andrewiam.com>
2018-01-24 12:07:11 -0800

    Fix parsing issues

--- MatrixWork.java
+++ MatrixWork.java
@@ -21,10 +21,12 @@ public class MatrixWork
          FileReader inputFile = new FileReader(file);
          BufferedReader lineBuffer = new BufferedReader(inputFile);
          matricies = scanFile(lineBuffer);
+         int[][] resultMatrix = matrixProduct(matricies.array1, matricies.array2);
+         printMatrix(resultMatrix);
       } catch (FileNotFoundException fileError) {
          System.out.println("ERROR: File not found!");
       } catch (IllegalArgumentException argError) {
-         System.out.println("ERROR: Could not parse file!");
+         System.out.println("ERROR: Could not process matrix!");
       } catch (IOException readError) {
          System.out.println("ERROR: Could not read file!");
       } catch (InputMismatchException readError) {
@@ -40,6 +42,7 @@ public class MatrixWork
       Scanner lineScanner1 = new Scanner(inputBuffer.readLine()).useDelimiter(" ");
       matricies.rows1 = lineScanner1.nextInt();
       matricies.cols1 = lineScanner1.nextInt();
+      matricies.array1 = new int[matricies.rows1][matricies.cols1];
       int current = 0;
       while((readLine = inputBuffer.readLine()) != null){ // Read size of first Matrix
          if(readLine.equals("")){ // Stop loop at empty line (need to scan next matrix)
@@ -55,12 +58,13 @@ public class MatrixWork
                shouldContinue = false;
             }
             col++;
-         }
+         } matricies.array2 = new int[matricies.rows2][matricies.cols2];
          current++;
       }
       Scanner lineScanner2 = new Scanner(inputBuffer.readLine()).useDelimiter(" ");
       matricies.rows2 = lineScanner2.nextInt();
       matricies.cols2 = lineScanner2.nextInt();
+      matricies.array2 = new int[matricies.rows2][matricies.cols2];
       current = 0;
       while((readLine = inputBuffer.readLine()) != null){ // Read size of first Matrix
          if(readLine.equals("")){ // Stop loop at empty line (need to scan next matrix)
@@ -71,7 +75,7 @@ public class MatrixWork
          int col = 0;
          while(shouldContinue) {
             try {
-               matricies.array1[current][col] = matrixScanner.nextInt();
+               matricies.array2[current][col] = matrixScanner.nextInt();
             } catch (NoSuchElementException endOfLine) {
                shouldContinue = false;
             }
@@ -89,7 +93,7 @@ public class MatrixWork
       int brows = B.length;
       int bcols = B[0].length;
       int[][] C = new int[arows][bcols];
-      if (arows != bcols)
+      if (acols != brows)
       {
          throw new IllegalArgumentException();
       }
@@ -111,5 +115,14 @@ public class MatrixWork
          sum += A[row][i]*B[i][col];
       }
       return sum;
+   }
+
+   private static void printMatrix(int[][] matrixToPrint){
+      for(int i = 0; i < matrixToPrint.length; i++){
+         for(int j = 0; j < matrixToPrint[i].length; j++){
+            System.out.print(matrixToPrint[i][j] + " ");
+         }
+         System.out.println();
+      }
    }
 }
