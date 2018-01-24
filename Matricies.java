c268ba10a9cd77218b32d96f0b6b790ff94f3d98
Andrew LeDawson <andrew@andrewiam.com>
2018-01-24 11:48:41 -0800

    Basic file scanning implemented

---
+++ Matricies.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Andrew LeDawson on 1/24/2018.
+ */
+public class Matricies {
+    int cols1;
+    int cols2;
+    int rows1;
+    int rows2;
+    int[][] array1;
+    int[][] array2;
+}
--- MatrixWork.java
+++ MatrixWork.java
@@ -5,19 +5,83 @@
 **  1-22-2018
 */
 
+import java.io.*;
 import java.util.*;
 
 public class MatrixWork
 {
-   public static void main(String[] args)
+   public static void main (String[] args)
    {
       Scanner scanner = new Scanner(System.in);
       System.out.println("Enter the data file name");
       String fname = scanner.next();
       File file = new File(fname);
-      Scanner fScanner = new Scanner(file);
-      
+      Matricies matricies;
+      try {
+         FileReader inputFile = new FileReader(file);
+         BufferedReader lineBuffer = new BufferedReader(inputFile);
+         matricies = scanFile(lineBuffer);
+      } catch (FileNotFoundException fileError) {
+         System.out.println("ERROR: File not found!");
+      } catch (IllegalArgumentException argError) {
+         System.out.println("ERROR: Could not parse file!");
+      } catch (IOException readError) {
+         System.out.println("ERROR: Could not read file!");
+      } catch (InputMismatchException readError) {
+         System.out.println("ERROR: Could not parse file!");
+      } catch (NoSuchElementException readError) {
+         System.out.println("ERROR: Could not parse file!");
+      }
    }
+
+   public static Matricies scanFile(BufferedReader inputBuffer) throws IllegalArgumentException, IOException, InputMismatchException, NoSuchElementException{
+      Matricies matricies = new Matricies();
+      String readLine;
+      Scanner lineScanner1 = new Scanner(inputBuffer.readLine()).useDelimiter(" ");
+      matricies.rows1 = lineScanner1.nextInt();
+      matricies.cols1 = lineScanner1.nextInt();
+      int current = 0;
+      while((readLine = inputBuffer.readLine()) != null){ // Read size of first Matrix
+         if(readLine.equals("")){ // Stop loop at empty line (need to scan next matrix)
+            break;
+         }
+         Scanner matrixScanner = new Scanner(readLine).useDelimiter(" ");
+         boolean shouldContinue = true;
+         int col = 0;
+         while(shouldContinue) {
+            try {
+               matricies.array1[current][col] = matrixScanner.nextInt();
+            } catch (NoSuchElementException endOfLine) {
+               shouldContinue = false;
+            }
+            col++;
+         }
+         current++;
+      }
+      Scanner lineScanner2 = new Scanner(inputBuffer.readLine()).useDelimiter(" ");
+      matricies.rows2 = lineScanner2.nextInt();
+      matricies.cols2 = lineScanner2.nextInt();
+      current = 0;
+      while((readLine = inputBuffer.readLine()) != null){ // Read size of first Matrix
+         if(readLine.equals("")){ // Stop loop at empty line (need to scan next matrix)
+            break;
+         }
+         Scanner matrixScanner = new Scanner(readLine).useDelimiter(" ");
+         boolean shouldContinue = true;
+         int col = 0;
+         while(shouldContinue) {
+            try {
+               matricies.array1[current][col] = matrixScanner.nextInt();
+            } catch (NoSuchElementException endOfLine) {
+               shouldContinue = false;
+            }
+            col++;
+         }
+         current++;
+      }
+      return matricies;
+   }
+
    public static int[][] matrixProduct (int [][] A, int[][] B) 
    {
       int arows = A.length;
